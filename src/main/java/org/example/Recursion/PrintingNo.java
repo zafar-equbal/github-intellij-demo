83c5ee92487a39837bd18e2a2a0bcfe8fc732e22
Md Zafar Equbal <zequbal705@gmail.com>
2026-03-21 23:33:53 +0530

    Printing Number from 1 to N using Recursion commit and Reverse too..

--- src/main/java/org/example/Recursion/PrintingNo.java
+++ src/main/java/org/example/Recursion/PrintingNo.java
@@ -1,16 +1,26 @@
 package org.example.Recursion;
 
 public class PrintingNo {
+    //Printing Number From 1 to n;
     public static void number(int n){
         if(n==0) {
             return;
         }
-
             number(n - 1);
         System.out.println(n);
     }
 
+    //Printing Number From N to 1 (Reverse order)
+    public static void numberReverse(int n){
+        if(n==0) {
+            return;
+        }
+        System.out.println(n);
+        numberReverse(n - 1);
+    }
+
     public static void main(String[] args) {
-        number(10);
+       // number(10);
+        numberReverse(10);
     }
 }
