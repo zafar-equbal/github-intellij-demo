ede1a77b5a20f6717e4313a87a13d03c782e2f89
Md Zafar Equbal <zequbal705@gmail.com>
2026-03-26 23:44:23 +0530

    Print Star commit

---
+++ src/main/java/org/example/Pattern/PrintStar.java
@@ -0,0 +1,16 @@
+package org.example.Pattern;
+
+public class PrintStar {
+    public static void main(String[] args) {
+     pattern(5);
+    }
+    public static void pattern(int n){
+        for (int i = 0;i<n;i++){
+            for (int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
