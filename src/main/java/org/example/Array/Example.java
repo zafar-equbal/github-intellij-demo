b24a97b2fac8ba9bce69fbc4a3d66d1d9d435556
Md Zafar Equbal <zequbal705@gmail.com>
2026-03-31 00:03:25 +0530

    Array Example commit

---
+++ src/main/java/org/example/Array/Example.java
@@ -0,0 +1,15 @@
+package org.example.Array;
+
+public class Example {
+    public static void main(String[] args) {
+       int [] arr={10,20,30,40,50,60};
+       int sum=0;
+       for (int i =0;i<arr.length;i++){
+        sum=sum+arr[i];
+
+       }
+       System.out.println(sum);
+
+    }
+
+}
