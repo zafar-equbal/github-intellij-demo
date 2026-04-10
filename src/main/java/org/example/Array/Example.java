e79bb0f55d4d4bfc8437db023f5a46cb5dba8c73
Md Zafar Equbal <zequbal705@gmail.com>
2026-04-11 01:33:17 +0530

    While loop commit

--- src/main/java/org/example/Array/Example.java
+++ src/main/java/org/example/Array/Example.java
@@ -2,14 +2,10 @@ package org.example.Array;
 
 public class Example {
     public static void main(String[] args) {
-       int [] arr={10,20,30,40,50,60};
-       int sum=0;
-       for (int i =0;i<arr.length;i++){
-        sum=sum+arr[i];
-
-       }
-       System.out.println(sum);
-
+        int i=1;
+        while(i<=4){
+            System.out.println("Hello Zafar "+i);
+            i++;
+        }
     }
-
 }
