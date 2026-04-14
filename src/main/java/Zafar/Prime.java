eed9d29fbc6b860e2d35cad9390b59a299dd21d3
Md Zafar Equbal <zequbal705@gmail.com>
2026-04-14 12:00:00 +0530

    Prime Number

---
+++ src/main/java/Zafar/Prime.java
@@ -0,0 +1,16 @@
+package Zafar;
+
+
+public class Prime {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(3);
+        System.out.println(5);
+        System.out.println(7);
+        System.out.println(11);
+        System.out.println(13);
+        System.out.println(17);
+        System.out.println(19);
+    }
+}
