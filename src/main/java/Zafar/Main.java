53cf051d2b53d10d64548c4d1dc803fae9600355
Md Zafar Equbal <92514218+zafar-equbal@users.noreply.github.com>
2026-07-22 22:55:13 +0530

    Update Main.java

--- src/main/java/Zafar/Main.java
+++ src/main/java/Zafar/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         star(5);
     }
-    public static void star(int n){
+    public static void star1(int n){
         for(int i=0;i<=n;i++){
-            for(int j=1;j<=i;j++){
+            for(int j=1;j<n;j++){
                 System.out.print("*");
             }
             
@@ -16,4 +16,13 @@ public class Main {
    
     }
 
+    public static void star2(int n){
+
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+    }
+            System.out.println();
+
+       }
 }
